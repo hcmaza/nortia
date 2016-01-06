5d73e4ce746837b57566e1947b7964486aa1eb91
Martin Paez Yañez <martinpy@gmail.com>
2016-01-05 21:31:18 -0300

    Correcciones en el alta de idea-proyecto, validacion de CUIT de comitente, areas de texto en documentacion, filtros en tabla de convocatoriasy la participacion en el equipo de trabajo

--- src/main/java/ar/edu/undec/nortia/controller/ConvocatoriaFacade.java
+++ src/main/java/ar/edu/undec/nortia/controller/ConvocatoriaFacade.java
@@ -7,10 +7,12 @@
 package ar.edu.undec.nortia.controller;
 
 import ar.edu.undec.nortia.model.Convocatoria;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 /**
@@ -32,12 +34,38 @@ public class ConvocatoriaFacade extends AbstractFacade<Convocatoria> {
     }
     
      public List<Convocatoria> findConvocatoriafinanciamiento(Integer tipoproyectoid,Integer tipofinanciamientoid){
-         String queryStr = "Select c from Convocatoria c where c.tipofinanciamientoid.id = :tipofinanciamientoid ";
-            TypedQuery<Convocatoria> query = getEntityManager().createQuery(queryStr, Convocatoria.class);
-            query.setParameter("tipofinanciamientoid", tipofinanciamientoid);
+        String queryStr = "Select c from Convocatoria c where c.tipofinanciamientoid.id = :tipofinanciamientoid ";
+        TypedQuery<Convocatoria> query = getEntityManager().createQuery(queryStr, Convocatoria.class);
+        query.setParameter("tipofinanciamientoid", tipofinanciamientoid);
             
       
         return query.getResultList();
+    }
+    
+    /*
+     * Método que busca las convocatorias abiertas (entre fecha de inicio y fecha de fin) en una fecha determinada.
+     */
+    public List<Convocatoria> findConvocatoriasEnFecha(Date fecha){
+         
+        TypedQuery query = this.em.createQuery("SELECT c FROM Convocatoria c WHERE :fecha BETWEEN c.fechainicio AND c.fechacierre", Convocatoria.class);
+        query.setParameter("fecha", fecha);
+        return query.getResultList();
+    }
+    
+    public List<Convocatoria> findConvocatoriasPorTipoProyectoTipoFinanciamientoYFecha(int tipoproyectoid, int tipofinanciamiento, Date fecha){
+        
+        String query = 
+                "SELECT c FROM Convocatoria c "
+                + "WHERE (:fecha BETWEEN c.fechainicio AND c.fechacierre) "
+                + "AND c.tipofinanciamientoid.id = :tipofinanciamiento "
+                + "AND c.tipoproyectoid.id = :tipoproyecto";
+        
+        TypedQuery consulta = this.em.createQuery(query, Convocatoria.class);
+        consulta.setParameter("fecha", fecha);
+        consulta.setParameter("tipofinanciamiento", tipofinanciamiento);
+        consulta.setParameter("tipoproyecto", tipoproyectoid);
+        
+        return consulta.getResultList();
     }
     
 }
